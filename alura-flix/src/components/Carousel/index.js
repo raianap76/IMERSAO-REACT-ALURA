19ed77e305a791465031dfafdb5f05571f2ee2e3
raianap76 <raianap76@gmail.com>
2020-07-30 22:28:05 -0300

    "Imersao Usando Slider para carrosel"

--- alura-flix/src/components/Carousel/index.js
+++ alura-flix/src/components/Carousel/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { VideoCardGroupContainer, Title, ExtraLink,VideoCardList } from './styles';
+import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
-//import Slider, { SliderItem } from './components/Slider';
+import Slider, { SliderItem } from './components/Slider';
 
 function Carousel({
   ignoreFirstVideo,
@@ -25,23 +25,23 @@ function Carousel({
           }
         </>
       )}
-      <VideoCardList>
+      <Slider>
         {videos.map((video, index) => {
           if (ignoreFirstVideo && index === 0) {
             return null;
           }
 
           return (
-            <li key={video.titulo}>
+            <SliderItem key={video.titulo}>
               <VideoCard
                 videoTitle={video.titulo}
                 videoURL={video.url}
                 categoryColor={categoryColor}
               />
-            </li>
+            </SliderItem>
           );
         })}
-      </VideoCardList>
+      </Slider>
     </VideoCardGroupContainer>
   );
 }
